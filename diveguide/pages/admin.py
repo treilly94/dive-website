0a08141e68089b6e7a27ca222ad51c675287bbdc
Tom Reilly <treilly94@live.com>
2017-05-14 15:47:38 +0100

    removed question and choice

--- diveguide/pages/admin.py
+++ diveguide/pages/admin.py
@@ -1,23 +1,7 @@
 from django.contrib import admin
 
-from .models import Question, Choice, Location
+from .models import Location
 
-class ChoiceInline(admin.TabularInline):
-    model = Choice
-    extra = 3
-
-
-class QuestionAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
-    ]
-    inlines = [ChoiceInline]
-    list_display = ('question_text', 'pub_date', 'was_published_recently')
-    list_filter = ['pub_date']
-    search_fields = ['question_text']
-
-admin.site.register(Question, QuestionAdmin)
 
 class LocationAdmin(admin.ModelAdmin):
     fieldsets = [
