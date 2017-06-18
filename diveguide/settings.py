c75805f6aebe151bf92418b96154cbbb60999897
Tom Reilly <treilly94@live.com>
2017-06-18 16:34:12 +0100

    static syntax updtate

--- diveguide/settings.py
+++ diveguide/settings.py
@@ -135,4 +135,9 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'static')
+    os.path.join(PROJECT_ROOT, 'static'),
+]
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
