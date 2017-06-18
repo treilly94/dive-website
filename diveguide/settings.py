b0ef914a86133c1cc8f260c4eb422dbb07160f90
Tom Reilly <treilly94@live.com>
2017-06-18 16:30:31 +0100

    static change

--- diveguide/settings.py
+++ diveguide/settings.py
@@ -136,7 +136,3 @@ STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
     os.path.join(PROJECT_ROOT, 'static'),
-
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
