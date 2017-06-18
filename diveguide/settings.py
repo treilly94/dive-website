2314612357d54bbd258b3a477611748cb55c7549
Tom Reilly <treilly94@live.com>
2017-06-18 16:32:10 +0100

    updated static

--- diveguide/settings.py
+++ diveguide/settings.py
@@ -135,4 +135,4 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'static')
