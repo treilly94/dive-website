9c0e81ea7d96d33c6793c36f4ded57a716d617bb
treilly94 <treilly94@live.com>
2018-01-16 16:23:28 +0000

    added test for detail

--- diveguide/sites/tests.py
+++ diveguide/sites/tests.py
@@ -1,12 +1,15 @@
 """
 This Module contains the tests for the pages app
 """
-import datetime
-from django.utils import timezone
 from django.test import TestCase
 from django.urls import reverse
 
 from .models import Location
+
+
+def create_site(location_name):
+
+    return Location.objects.create(location_name=location_name)
 
 
 class RenderTests(TestCase):
@@ -17,3 +20,11 @@ class RenderTests(TestCase):
         self.assertTemplateUsed(response, 'sites/index.html')
         self.assertContains(response, 'Fresh water sites')
         self.assertContains(response, 'Salt water sites')
+
+    def test_detail_page(self):
+        site = create_site("Trefor Pier")
+        url = reverse('sites:detail', args=(site.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'sites/detail.html')
+        self.assertContains(response, site.location_name)
