1ca34b894fab6b14296854d53275041f8a5f0613
treilly94 <treilly94@live.com>
2018-01-16 16:58:44 +0000

    added test for index with site

--- diveguide/sites/tests.py
+++ diveguide/sites/tests.py
@@ -7,13 +7,14 @@ from django.urls import reverse
 from .models import Location
 
 
-def create_site(location_name):
+def create_site(**kwargs):
 
-    return Location.objects.create(location_name=location_name)
+    return Location.objects.create(**kwargs)
 
 
-class RenderTests(TestCase):
-    def test_index_page(self):
+class IndexViewTests(TestCase):
+
+    def test_index_page_empty(self):
         url = reverse('sites:index')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
@@ -21,8 +22,22 @@ class RenderTests(TestCase):
         self.assertContains(response, 'Fresh water sites')
         self.assertContains(response, 'Salt water sites')
 
+    def test_index_page_sw(self):
+        kwargs = {"location_name": "Trefor Pier",
+                  "water_type": "SW"}
+        site = create_site(**kwargs)
+        url = reverse('sites:detail', args=(site.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'sites/detail.html')
+        self.assertContains(response, site.location_name)
+
+
+class DetailViewTests(TestCase):
+
     def test_detail_page(self):
-        site = create_site("Trefor Pier")
+        kwargs = {"location_name":"Trefor Pier"}
+        site = create_site(**kwargs)
         url = reverse('sites:detail', args=(site.id,))
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
