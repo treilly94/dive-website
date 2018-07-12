fdbfe89d20334c8ebc1af5eba87112773100d381
treilly94 <treilly94@live.com>
2018-07-12 11:04:21 +0100

    Removed heroku files

--- diveguide/diveguide/settings.py
+++ diveguide/diveguide/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'HOST': 'db',
+        'HOST': 'localhost',
         'PORT': 5432,
     }
 }
